240cfc3a61e5b540554ebaa4e2b9d799c41be7b8
Jose carlos Rodriguez <jcrodriguez@opentrends.net>
2025-11-18 16:46:12 +0100

    repo & dataSource

---
+++ toktik/lib/infraestructure/repositories/video_posts_repositiry_impl.dart
@@ -0,0 +1,24 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostsRepositoryImpl implements VideoPostRepository {
+
+  final VideoPostsDatasource videosPostsDatasource;
+
+  VideoPostsRepositoryImpl({
+    required this.videosPostsDatasource
+  });
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosPostsDatasource.getTrendingVideosByPage( page );
+
+  }
+
+}
